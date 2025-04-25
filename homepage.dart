a0a38135af38c532b43fb701cc48bc97e5548fec
Elaine Victoria <111344174+spacedecors@users.noreply.github.com>
2025-04-25 09:34:27 +0800

    Create homepage.dart

---
+++ homepage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class WidgetBot extends StatefulWidget {
+  const WidgetBot({Key? key}) : super(key: key);
+
+  @override
+  _WidgetBotState createState() => _WidgetBotState();
+}
+
+class _WidgetBotState extends State<WidgetBot> {
+  int _currentIndex = 0;
+
+  List<Widget> body = const [
+    Icon(Icons.menu),
+    Icon(Icons.map),
+    Icon(Icons.loyalty),
+    Icon(Icons.person),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: body[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Menu',
+            icon: Icon(Icons.menu),
+          ),
+          BottomNavigationBarItem(
+            label: 'Maps',
+            icon: Icon(Icons.map),
+          ),
+          BottomNavigationBarItem(
+            label: 'Loyalty',
+            icon: Icon(Icons.loyalty),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
