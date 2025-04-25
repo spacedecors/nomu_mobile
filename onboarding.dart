cd0eeae63e661fd37892c388c19cb0742ce4fedb
Elaine Victoria <111344174+spacedecors@users.noreply.github.com>
2025-04-25 09:35:32 +0800

    Create onboarding.dart

---
+++ onboarding.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'content_model.dart';
+
+class Onboarding extends StatefulWidget {
+      final Map userData; // This should include 'name', 'points', and 'goods' data
+
+  const Onboarding({Key? key, required this.userData}) : super(key: key);
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  int currentIndex = 0;
+  late PageController _controller;
+
+  @override
+  void initState() {
+    _controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _controller,
+              itemCount: contents.length,
+              onPageChanged: (int index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              itemBuilder: (_, i) {
+                return Padding(
+                  padding: const EdgeInsets.all(40),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        contents[i].image,
+                        height: 300,
+                      ),
+                      Text(
+                        contents[i].title,
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        contents[i].description,
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              contents.length,
+              (index) => buildDot(index, context),
+            ),
+          ),
+          Container(
+            height: 60,
+            margin: EdgeInsets.all(40),
+            width: double.infinity,
+            child: TextButton(
+              child: Text(
+                  currentIndex == contents.length - 1 ? "Get Started" : "Next"),
+              onPressed: () {
+                if (currentIndex == contents.length - 1) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => UserTypeSelection(userData: widget.userData),
+                    ),
+                  );
+                } else {
+                  _controller.nextPage(
+                    duration: Duration(milliseconds: 100),
+                    curve: Curves.bounceIn,
+                  );
+                }
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Color(0xFF40A858), // Change background color here
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container buildDot(int index, BuildContext context) {
+    return Container(
+      height: 10,
+      width: currentIndex == index ? 25 : 10,
+      margin: EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color(0xFF40A858), // Use the same color here
+      ),
+    );
+  }
+}
